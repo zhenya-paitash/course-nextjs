00a45da6b9f9581f7db02af2a986c58989076475
zhenya-paitash <naitavv93@gmail.com>
2021-04-14 17:51:57 +0300

    3. Config - Adding Theme UI

--- 1.Introduction to Next.js (2020)/pages/_app.jsx
+++ 1.Introduction to Next.js (2020)/pages/_app.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import '../src/styles.module.css'
+import { ThemeProvider } from 'theme-ui'
+import theme from '../theme'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
