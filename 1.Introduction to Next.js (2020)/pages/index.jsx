d9af3dcfe1b6c37a4aac7f2b40b52dea198c9a05
zhenya-paitash <naitavv93@gmail.com>
2021-04-16 14:23:49 +0300

    4. API - Fetching Notes & Dynamic Rendering

--- 1.Introduction to Next.js (2020)/pages/index.jsx
+++ 1.Introduction to Next.js (2020)/pages/index.jsx
@@ -1,12 +1,24 @@
 /** @jsxImportSource theme-ui */
 import Link from 'next/link'
 
-const _ = () => (
+const _ = ({ content }) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
-      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+      <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
     </div>
   </div> 
 )
 
 export default _
+
+export function getStaticProps() {
+  // get data from CMS
+
+  return {
+    props: {
+      content: {
+        title: 'This is my really nice app 🤣',
+      }
+    }
+  }
+}
--- 1.Introduction to Next.js (2020)/pages/notes/[id].jsx
+++ 1.Introduction to Next.js (2020)/pages/notes/[id].jsx
@@ -2,15 +2,31 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-const _ = () => {
-  const router = useRouter()
-  const { id }= router.query
-
+const _ = ({ note }) => {
   return (
     <div sx={{variant: 'containers.page'}}>
-      <h1>Note: {id} </h1>
+      <h1>Note: {note?.title} </h1>
     </div>
   )
 }
 
 export default _
+
+export async function getServerSideProps({params, req, res}) {
+  console.log(params)
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+
+  if (!response.ok) {
+    // res.writeHead(304, { Location: '/notes' }).end()
+    return {props: {}}
+  }
+
+  const {data} = await response.json()
+  
+  
+  if (data) {
+    return {
+      props: {note: data}
+    }
+  }
+}
